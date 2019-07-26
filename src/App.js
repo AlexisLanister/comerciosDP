908a143e0b168be9afaafce48d9398e978fe2233
Alexis <alexis.fereira@payco.co>
2019-07-26 18:00:16 -0500

    nuevo proyecto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,149 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import styled from 'styled-components';
+import {comercios} from "./comercios";
+
+
+let NARANJA = "#ff5700";
+
+
+const Banner = styled.div`
+  border-bottom:1px solid transparent;
+`
+
+const Comercio= styled.section`
+
+   transition: all 500ms cubic-bezier(0.190, 1.000, 0.220, 1.000);
+   
+.comercio{
+  border:1px solid #e4e4e4;
+  overflow: hidden;
+  position: relative;
+  display: block;
+   transition: all 500ms cubic-bezier(0.190, 1.000, 0.220, 1.000);
+
+  .recuadro{
+    opacity: .2;
+  }
+
+  .back-enlace{
+    position: absolute;
+    top: 100%;
+    left: 0;
+    background-color: white;
+    border-radius: 50px;
+    transform: translate(10%,50%) rotate(0deg) scale(1);
+    z-index: 2;
+    width: 100%;
+    height: 100%;
+     transition: all 500ms cubic-bezier(0.190, 1.000, 0.220, 1.000);
+  }
+
+  p,a{
+    position: absolute;
+    display: block;
+    top: 0;
+    left: 0;
+  }
+
+  a{
+    top:auto;
+    bottom: 0;
+    color: white;
+    z-index: 5;
+    padding: 1em;
+    left: auto;
+    right:0;
+    transform:translateX(-100%);
+    opacity:0;
+    transition: all 500ms cubic-bezier(0.190, 1.000, 0.220, 1.000);
+    
+    h4{font-size:18px; color:${NARANJA}}
+
+  }
+  
+  &:hover{
+    transform:scale(.9);
+      background:${NARANJA};
+  
+    .back-enlace{
+      transform: translate(10%,0%) rotate(76deg) scale(1.5);
+    }
+    a{
+    transform:translateX(0);
+    opacity:1;
+    }
+  }
+
+}
+`
+
+const COMERCIO= ({url,img,nombre})=>{
+  return(
+      <Comercio className={"col-6 col-md-4 col-lg-3 p-1 "}>
+        <div className="comercio">
+        <p>{nombre}</p>
+         <img src={img} className="imgr" alt=""/>
+          <div className="back-enlace"></div>
+          <a href={url} target={"_blank"}><h4><i>Ir al comercio</i></h4></a>
+        </div>
+      </Comercio>
+  )
+
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+      <div className="main-container">
+
+        <div className="content">
+          <header className="py-3 bgo wc">
+            <div className="container f01 jcsb px-md-0">
+              <div className="logo mt-2 " style={{flex:"0 0 120px",maxwidth: "180px"}}>
+                <img src="img/epayco.png" alt="" style={{maxWidth:"100%"}}/>
+              </div>
+              <div className="logo l02  text-right" style={{flex:"1 0 120px",maxWidth:"300px"}}>
+                <img src="img/davivienda.png" alt="" style={{maxWidth:"100%",maxwidth: "200px"}}/>
+              </div>
+            </div>
+          </header>
+
+
+          <Banner>
+            <div className="container px-md-3 px-0">
+                <img src="/img/banner.jpg" alt="" className={"imgr"}/>
+            </div>
+          </Banner>
+
+          <div className="container py-4">
+            <div className={"f01"}>
+              {comercios.map(item =>
+                  <COMERCIO
+                      url={item.web}
+                      nombre={item.url}
+                      img={item.img}
+                  />
+              )}
+            </div>
+          </div>
+
+          <footer className="py-3 bgb wc ">
+            <div className="container f01 px-md-0">
+              <div className="container f01 jcsb px-md-0">
+                <div className="logo mt-2">
+                  <img src="img/epayco.png" alt=""/>
+                </div>
+                <div className="logo l02">
+                  <img src="img/davivienda.png" alt=""/>
+                </div>
+              </div>
+            </div>
+          </footer>
+        </div>
+      </div>
+
   );
 }
 
---
+++ src/comercios.js
@@ -0,0 +1,231 @@
+export const comercios =
+    [
+            {
+                "web": "https://encarguelo.com/",
+                "Categoria": "Tecnologia y varios",
+                "img": "img/comercios/encarguelo.png",
+                "nombre":"Encarguelo"
+            },
+            {
+                "web": " http://wanitta.com/",
+                "Categoria": "Ropa ",
+                "img": "img/comercios/wanita.png",
+                "nombre":"Wanita"
+            },
+            {
+                "web": "http://www.veganomarket.com.co",
+                "Categoria": "Alimentos ",
+                "img": "img/comercios/vegano-market.png",
+                "nombre":"Vegan Market"
+            },
+            {
+                "web": "http://luxecubancigars.com",
+                "Categoria": "Tabaco ",
+                "img": "img/comercios/luxe-cuban.png",
+                "nombre":"Luxe Cuban Cigars"
+            },
+            {
+                "web": "http://www.medellinadventuretrails.com/index.php/es/",
+                "Categoria": "Experiencias ",
+                "img": "img/comercios/adventure-trails.png",
+                "nombre":"Medellin Adventure Trails"
+            },
+            {
+                "web": "https://santisimasropaintima.com/",
+                "Categoria": "Ropa interior",
+                "img": "img/comercios/santisimas.png",
+                "nombre":"Santisimas spa"
+            },
+            {
+                "web": "https://www.vintrostore.com/",
+                "Categoria": "Bolsos",
+                "img": "img/comercios/vintro.png",
+                "nombre":"Vintro"
+            },
+            {
+                "web": "https://sexosentido.co/tienda/es/",
+                "Categoria": "Sex shop",
+                "img": "img/comercios/sexosentido.png",
+                "nombre":"Sexosentido"
+            },
+            {
+                "web": "http://www.catafitclothes.com/",
+                "Categoria": "Ropa deportiva",
+                "img": "img/comercios/catazuleta.png",
+                    "nombre":"Enca"
+            },
+            {
+                "web": "https://colsubastas.com/",
+                "Categoria": "Espacio publicitario en web",
+                "img": "img/comercios/colsubastas.png",
+                "nombre":"Enca"
+            },
+            {
+                "web": "http://www.brainystore.com.co",
+                "Categoria": "Lentes ",
+                "img": "img/comercios/brainy.png",
+                "nombre":"Enca"
+            },
+            {
+                "web": "http://animalista.com.co",
+                "Categoria": "Calzado, bolsos y accesorios",
+                "img": "img/comercios/animalista.png",
+                "nombre":"Enca"
+            },
+            {
+                "web": "http://acontar.co",
+                "Categoria": "Cursos de contabilidad",
+                "img": "img/comercios/acontar.png",
+                "nombre":"Acontar"
+            },
+            {
+                "web": "http://pakopeluches.com",
+                "Categoria": "Peluches",
+                "img": "img/comercios/pako.png",
+                "nombre":"Enca"
+            },
+            {
+                "web": "http://www.biosil.co",
+                "Categoria": "Complejo vitamínico ",
+                "img": "img/comercios/biosil.png",
+                "nombre":"Enca"
+            },
+            {
+                "web": "https://floristeriasaritavirtual.com",
+                "Categoria": "Floristería",
+                "img": "img/comercios/sarita.png",
+                "nombre":"Enca"
+            },
+            {
+                "web": "fajasbodyflex.com",
+                "Categoria": "Fajas ",
+                "img": "img/comercios/bodyflex.png",
+                "nombre":"Enca"
+            },
+            {
+                "web": "http://www.mwhitestore.com",
+                "Categoria": "Store",
+                "img": "img/comercios/mwhite.png",
+                "nombre":"Enca"
+            },
+            {
+                "web": "http://www.dulcefruto.com",
+                "Categoria": "Desayunos, sorpresas",
+                "img": "img/comercios/dulce-fruto.png",
+                "nombre":"Enca"
+            },
+            {
+                "web": "http://www.medicolsa.com.co/",
+                "Categoria": "Equipos de diagnóstico medico",
+                "img": "img/comercios/medicolsa.png",
+                "nombre":"Medicolsa"
+            },
+            {
+                "web": "www.floristeriamomentofloral.co",
+                "Categoria": "Floristería",
+                "img": "img/comercios/momento-floral.png",
+                    "nombre":"Momento Floral"
+            },
+            {
+                "web": "https://frutosalvaje.com/",
+                "Categoria": "Productos capilares",
+                "img": "img/comercios/fruto-salvaje.png",
+                "nombre":"Enca"
+            },
+            {
+                "web": "http://www.esmeraldascolombia.com",
+                "Categoria": "Joyeria",
+                "img": "img/comercios/esmeraldas-colombia.png",
+                "nombre":"Enca"
+            },
+            {
+                "web": "http://www.ilustre.co",
+                "Categoria": "Eventos culturales",
+                "img": "img/comercios/illustre.png",
+                "nombre":"Enca"
+            },
+            {
+                "web": "https://macadamiastore.com/",
+                "Categoria": "Calzado",
+                "img": "img/comercios/macadamia.png",
+                "nombre":"Enca"
+            },
+            {
+                "web": "https://guerreroguane.com/",
+                "Categoria": "Eventos de ciclismo",
+                "img": "img/comercios/guerrero-guane.png",
+                "nombre":"Enca"
+            },
+            {
+                "web": "http://www.puroyorganico.com.co",
+                "Categoria": "Productos naturales",
+                "img": "img/comercios/puro-organico.png",
+                "nombre":"Enca"
+            },
+            {
+                "web": "http://www.colorful.com.co",
+                "Categoria": "Cuadernos",
+                "img": "img/comercios/colorful.png",
+                "nombre":"Enca"
+            },
+            {
+                "web": "https://www.celuganga.co/",
+                "Categoria": "Celulares y accesorios",
+                "img": "img/comercios/celuganga.png",
+                "nombre":"Enca"
+            },
+            {
+                "web": "http://mundoreymon.com.co",
+                "Categoria": "Ropa interior",
+                "img": "img/comercios/reymon.png",
+                "nombre":"Enca"
+            },
+            {
+                "web": "www.detallesalinstante.com.co",
+                "Categoria": "Detalles",
+                "img": "img/comercios/detalles-instante.png",
+                "nombre":"Enca"
+            },
+            {
+                "web": "https://www.hardsalsabogota.com",
+                "Categoria": "Música",
+                "img": "img/comercios/hard-salsa.png",
+                "nombre":"Enca"
+            },
+            {
+                "web": "http://www.akzaraspa.com",
+                "Categoria": "Spa",
+                "img": "img/comercios/akzara.png",
+                "nombre":"Enca"
+            },
+            {
+                "web": "Https://www.bikeexchange.com.co",
+                "Categoria": "Bicicletas y accesorios",
+                "img": "img/comercios/bike.png",
+                "nombre":"Enca"
+            },
+            {
+                "web": "http://www.royalticket.co",
+                "Categoria": "Tickets eventos",
+                "img": "img/comercios/royalticket.png",
+                "nombre":"Enca"
+            },
+            {
+                "web": "http://chocotriunfo.com.co/",
+                "Categoria": "Chocolates",
+                "img": "img/comercios/triunfo.png",
+                "nombre":"Triunfo"
+            },
+            {
+                "web": "https://dfvleather.com/",
+                "Categoria": "Calzado y bolsos",
+                "img": "img/comercios/dfv.png",
+                "nombre":"DFV"
+            },
+            {
+                "web": "http://donguillermoruiz.com",
+                "Categoria": "Libros deportivos",
+                "img": "img/comercios/dgr.png",
+                "nombre":"Don Guillermo Ruiz"
+            }
+        ];
